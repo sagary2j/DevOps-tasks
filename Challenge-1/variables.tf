350ceac7c68542c5ac7ccfeceb198b1778c94417
Sagar R <sagar.ravkhande@gmail.com>
2021-07-11 18:12:56 +0530

    updated tf files for 3 tier architecture in aws

---
+++ Challenge-1/variables.tf
@@ -0,0 +1,11 @@
+variable "user_data_file_pub" {
+  default = "install_apache.sh"
+}
+
+variable "db_user" {
+  default = "azureuser"
+}
+
+variable "db_password" {
+  default = "dR2ctKdQGguYJAX2"
+}
